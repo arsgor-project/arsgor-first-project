b67dc3127e35c16a2606dced013f9894ef0b8ed2
arsgor <arsgorynin@yandex.ru>
2020-10-04 19:26:04 +0700

    check and compile

--- test_problem_3.sh
+++ test_problem_3.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ./MinimalFEM test3.inp test3.out
-python scripts/PostProcess.py test3.inp test3.out
+python3 scripts/PostProcess.py test3.inp test3.out
--- test_problem_4.sh
+++ test_problem_4.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ./MinimalFEM test4.inp test4.out
-python scripts/PostProcess.py test4.inp test4.out
+python3 scripts/PostProcess.py test4.inp test4.out
